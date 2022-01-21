e6829597f7131091d70ade90445735e353dab38c
Shivam Aswal <92840731+shivamaswal@users.noreply.github.com>
2022-01-21 19:29:41 +0530

    Delete ShivamAswal directory

--- ShivamAswal/javascript/index.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".no-legend").addEventListener("click",function(){
-    document.querySelector("img").setAttribute("src","./images/btnhp.jpg");
-    // document.querySelector("body").style.backgroundColor="#fff";
-    for(var i=0;i<document.querySelectorAll("button").length;i++){
-        document.querySelectorAll("button")[i].style.visibility="hidden";
-    }
-    document.querySelector("img").classList.add("img-lg");
-});
--- ShivamAswal/javascript/main.js
+++
@@ -1,7 +0,0 @@
-function randomNumber(){
-    var number = Math.floor(Math.random()*7)+1;
-    console.log(number);
-    return number;
-}
-// console.log(Math.floor(Math.random()*7+1));
-document.querySelector(".title-img").setAttribute("src","../images/hello"+randomNumber()+".png");
